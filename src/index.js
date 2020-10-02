640398252f07f4563eb4c1db1b61f35f8de3c6e6
Aleksandr Kuzmin <kuzmin@nextmail.ru>
2020-10-03 00:35:32 +0400

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array === undefined || array.length === 0) return 0;
+  return array.reduce(function (value1, value2) {
+    return ( value1 < value2 ? value1 : value2 );
+  });
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array === undefined || array.length === 0) return 0;
+  return array.reduce(function (value1, value2) {
+    return ( value1 > value2 ? value1 : value2 );
+  });
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array === undefined || array.length === 0) return 0;
+  let sum = array.reduce((sum, elem) => sum + elem);
+  return sum / array.length;
 }
